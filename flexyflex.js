0f5ec2afc868b05c809bd386c8a2cf9926f6cd53
Joaquim Perez <jquim1@hotmail.com>
2018-01-18 13:45:54 +0000

    Maior separação dos ficheiros

--- flexyflex.js
+++ flexyflex.js
@@ -1,69 +1,9 @@
 /** BIBLIOTECAS **/
 const io = require("./io.js");
+const menu = require("./menus.js");
 /*********************/
 
-/** MENU PRINCIPAL **/
-const mprincipal = 
-[ 
-	{
-		label: "Geração de um número aleatório",
-		action: () =>
-		{
-			io.out("Número aleatório: "+Math.random());
-		}
-	},
-	{
-		label: "Guess-a-guess",
-		action: () =>
-		{
-			const high = 5;
-			const low = 1;		
-			io.out("O objetivo é acertar num número determinado aleatoriamente entre " + low + " e " + high);
-
-			var solution = Math.floor(Math.random() * (high - low) + low);
-			
-			while(io.getin("Introduza um número entre 1 e 5: ") != solution);
-			
-			io.out("Acertaste!");
-		}
-	},
-	{
-		label: "Diálogo com um bot",
-		action: () =>
-		{
-			io.menu("BOT",bot);
-		}
-	} 
-];
-const bot =
-[
-	{
-		label: "Olá",
-		action: () =>
-		{
-			io.out("Olá bebé");
-		}
-	},
-	{
-		label: "Oi",
-		action: () =>
-		{
-			io.out("Oi, tb?");
-		}
-	},
-	{
-		label: "Hello",
-		action: () =>
-		{
-			io.out("Hello darling");
-		}
-	}
-];
-/************************/
-/************************/
-/************************/
-/************************/
-/************************/
-/************************/
-/************************/
-io.menu("MENU PRINCIPAL",mprincipal);
+/*******************/
+/** EXECUÇÃO **/
+/*******************/
+io.menu("MENU PRINCIPAL",menu.principal);
---
+++ menus.js
@@ -0,0 +1,61 @@
+/** BIBLIOTECAS **/
+const io = require("./io.js");
+/*********************/
+
+module.exports.principal =
+[ 
+	{
+		label: "Geração de um número aleatório",
+		action: () =>
+		{
+			io.out("Número aleatório: "+Math.random());
+		}
+	},
+	{
+		label: "Guess-a-guess",
+		action: () =>
+		{
+			const high = 5;
+			const low = 1;		
+			io.out("O objetivo é acertar num número determinado aleatoriamente entre " + low + " e " + high);
+
+			var solution = Math.floor(Math.random() * (high - low) + low);
+			
+			while(io.getin("Introduza um número entre 1 e 5: ") != solution);
+			
+			io.out("Acertaste!");
+		}
+	},
+	{
+		label: "Diálogo com um bot",
+		action: () =>
+		{
+			io.menu("BOT",bot);
+		}
+	} 
+];
+
+module.exports.bot =
+[
+	{
+		label: "Olá",
+		action: () =>
+		{
+			io.out("Olá bebé");
+		}
+	},
+	{
+		label: "Oi",
+		action: () =>
+		{
+			io.out("Oi, tb?");
+		}
+	},
+	{
+		label: "Hello",
+		action: () =>
+		{
+			io.out("Hello darling");
+		}
+	}
+];
